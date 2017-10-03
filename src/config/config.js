ab427450d7a4a7265e27a48e256de30a96ad79fb
Floyd Noel <floydnoel@Floyds-MacBook-Pro.local>
2017-10-03 08:45:16 -0600

    added production config

---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+export default {
+  authProvider: "google",
+  apiKey: "AIzaSyBeTLRb5SUIwAINZRYguBqVVJ9PxMtHjCQ",
+  authDomain: "octopus-wiki-39bd4.firebaseapp.com",
+  databaseURL: "https://octopus-wiki-39bd4.firebaseio.com",
+  projectId: "octopus-wiki-39bd4",
+  storageBucket: "",
+  messagingSenderId: "718622896400"
+};
